d4c67f38576996cf4fcf2e9584b678b1566b5684
Anne-Marie Jayatilake <annemarie.jay20@gmail.com>
2021-08-05 13:40:32 +1000

    commiting

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCss = require('optimize-css-assets-webpack-plugin');
 
 
 module.exports = {
@@ -19,8 +21,22 @@ module.exports = {
                     enforce: true
                 }
             }
-        }
+        },
+        minimizer:  [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6
+                }
+            }),
+            new OptimizeCss({
+                cssProcessorOptions: {
+                    discardComments: true
+                }
+            })
+        ],
     },
+ 
     devServer: {
         hot: true,
         compress: true,
